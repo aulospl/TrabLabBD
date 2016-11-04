01b100dfbd6fac3bdcd5b30d10e4377f7c11b7b5
aulos <aulospl@gmail.com>
2016-11-04 13:33:11 -0200

    Refactored FXMLDocument and controller to TableDoc

--- src/emissoralabbd/EmissoraLabBD.java
+++ src/emissoralabbd/EmissoraLabBD.java
@@ -19,7 +19,7 @@ public class EmissoraLabBD extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("TableDoc.fxml"));
         
         Scene scene = new Scene(root);
         
